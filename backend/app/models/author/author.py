d3e8595b24532a87fd824517097c99f0dff82be9
lewelyn7 <karol.hamielec@gmail.com>
2022-12-17 13:51:47 +0100

    add models

--- backend/app/models/author/author.py
+++ backend/app/models/author/author.py
@@ -1,1 +1,29 @@
+from app.database import Base
+from sqlalchemy import Column, String, Integer, BigInteger
+from sqlalchemy.orm import relationship
+from pydantic import BaseModel
  
+class AuthorDB(Base):
+    __tablename__ = "authors"
+    id = Column(String(25), primary_key=True, index=True)
+    num_of_games_owned = Column(Integer)
+    num_reviews = Column(Integer)
+    playtime_forever = Column(BigInteger)
+    playtime_last_two_weeks = Column(BigInteger)
+
+    reviews = relationship("ReviewDB", back_populates="author", lazy="dynamic")
+
+class AuthorBase(BaseModel):
+    num_of_games_owned: int
+    num_reviews: int
+    playtime_forever: int
+    playtime_last_two_weeks: int
+
+class AuthorCreate(AuthorBase):
+    pass
+
+class Author(AuthorBase):
+    id: str
+
+    class Config:
+        orm_mode = True
--- backend/app/models/review/review.py
+++ backend/app/models/review/review.py
@@ -1,1 +1,51 @@
- 
+from app.database import Base
+from sqlalchemy import Column, String, Integer, Boolean, DateTime, Float, BigInteger
+from sqlalchemy.orm import relationship
+from pydantic import BaseModel
+from datetime import datetime
+
+class ReviewDB(Base):
+    id = Column(String(25), primary_key=True, index=True)
+    language = Column(String(25))
+    content = Column(String(1000))
+    timestamp_created = Column(DateTime)
+    timestampe_updated = Column(DateTime)
+    recommended = Column(Boolean)
+    votes_helpful = Column(Integer)
+    votes_funny = Column(Integer)
+    weighted_vote_score = Column(Float)
+    comment_count = Column(Float)
+    steam_purchase = Column(Boolean)
+    received_for_free = Column(Boolean)
+    written_during_early_access = Column(Boolean)
+    playtime_at_review = Column(BigInteger)
+
+    author = relationship("AuthorDB", back_populates="reviews")
+    game = relationship("GameDB", back_populates="reviews")
+
+
+
+
+class ReviewBase(BaseModel):
+    language: str
+    content: str
+    timestamp_created: datetime
+    timestampe_updated: datetime
+    recommended: bool
+    votes_helpful: int
+    votes_funny: int
+    weighted_vote_score: float
+    comment_count: int
+    steam_purchase: bool
+    received_for_free: bool
+    written_during_early_access: bool
+    playtime_at_review: int
+
+class ReviewCreate(ReviewBase):
+    pass
+
+class Review(ReviewBase):
+    id: str
+
+    class Config:
+        orm_mode = True
