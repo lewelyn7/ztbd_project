90d5ad61b98f2d8f134ca20c015cdb47e4cdd420
lewelyn7 <karol.hamielec@gmail.com>
2023-01-29 01:51:02 +0100

    redis retry

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
 FROM node:18-alpine AS angular-build
 WORKDIR /usr/src/app
-COPY frontend/db-perf-tester/* ./
+COPY frontend/db-perf-tester/*.json ./
 RUN npx npm install
 RUN npx npm install @angular/cli
+COPY frontend/db-perf-tester/src ./src
 RUN ls -al
 RUN npx ng build --build-optimizer --base-href="/static/"
 
@@ -13,4 +14,5 @@ RUN pip install --upgrade pip
 COPY ./backend/requirements.txt /app/
 RUN pip install -r requirements.txt
 COPY ./backend/app /app
+COPY ./backend/.env /app/.env
 COPY --from=angular-build /usr/src/app/dist/db-perf-tester/* /app/static/
--- backend/app/databases/redis.py
+++ backend/app/databases/redis.py
@@ -6,21 +6,37 @@ from redis.commands.search.field import TextField, NumericField, TagField
 from redis.commands.search.indexDefinition import IndexDefinition, IndexType
 from redis.commands.search.query import NumericFilter, Query
 from redis import exceptions
+from redis.retry import Retry
+from redis.backoff import ConstantBackoff
+
 
 class RedisDbs(Enum):
     AUTHORS = 0
     GAMES = 0
     REVIEWS = 0
 
-redisInstanceAuthors = redis.Redis(settings.REDIS_HOST, settings.REDIS_PORT, db=RedisDbs.AUTHORS.value)
-redisInstanceGames = redis.Redis(settings.REDIS_HOST, settings.REDIS_PORT, db=RedisDbs.GAMES.value)
-redisInstanceReviews = redis.Redis(settings.REDIS_HOST, settings.REDIS_PORT, db=RedisDbs.REVIEWS.value)
-schema = (TextField("$.author_id", as_name="author_id"),TextField("$.game_id", as_name="game_id"))
+
+redisInstanceAuthors = redis.Redis(
+    settings.REDIS_HOST,
+    settings.REDIS_PORT,
+    db=RedisDbs.AUTHORS.value,
+    retry_on_error=[exceptions.BusyLoadingError],
+    retry=Retry(backoff=ConstantBackoff(3), retries=-1),
+)
+redisInstanceGames = redisInstanceAuthors
+redisInstanceReviews = redisInstanceAuthors
+schema = (
+    TextField("$.author_id", as_name="author_id"),
+    TextField("$.game_id", as_name="game_id"),
+)
 try:
-    info = redisInstanceReviews.ft('reviews_idx').info()
+    info = redisInstanceReviews.ft("reviews_idx").info()
 except exceptions.ResponseError as e:
     print("adding index")
-    redisInstanceReviews.ft('reviews_idx').create_index(schema, definition=IndexDefinition(prefix=["review:"], index_type=IndexType.JSON))
+    redisInstanceReviews.ft("reviews_idx").create_index(
+        schema,
+        definition=IndexDefinition(prefix=["review:"], index_type=IndexType.JSON),
+    )
 
 
 def get_redis(db: RedisDbs) -> redis.Redis:
